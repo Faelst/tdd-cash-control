145774ed0e845c3ad39b52f9717b8d0ca1e6f05f
Rafael Silverio <fael_st@hotmail.com>
2022-08-09 02:12:41 -0300

    simplify CacheStore inser a one method

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -18,6 +18,11 @@ export class CacheStoreSpy implements CacheStore {
         this.insertValues = value
     }
 
+    replace(key: string, value: any): void {
+        this.delete(key)
+        this.insert(key, value)
+    }
+
     simulateDeleteError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
             this.actions.push(CacheStoreSpy.Action.delete)
@@ -36,6 +41,7 @@ export class CacheStoreSpy implements CacheStore {
 export namespace CacheStoreSpy {
     export enum Action {
         delete,
-        insert
+        insert,
+        replace
     }
 }
